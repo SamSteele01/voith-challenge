fac3a9793f1ca1d618a2d7bf8e121d681049d07e
Sam Steele <ssteele017@gmail.com>
2022-08-24 19:46:06 -0400

    use components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import CurrentWeather from './CurrentWeather'
+
+const Background = styled.div`
+  text-align: center;
+  background-color: #282c34;
+  min-height: 100vh;
+`
+
+// mobile device width
+const Screen = styled.div`
+  background-color: white;
+  width: 400px;
+  padding: 20px;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Background>
+      <Screen>
+        <h2>Heidenheim, Germany</h2>
+        <CurrentWeather />
+      </Screen>
+    </Background>
   );
 }
 
